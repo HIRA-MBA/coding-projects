991b05ea9f24bffbacf98f3bbfd981cc82bb38ce
hira <hirashamimqureshi@gmail.com>
2024-04-19 21:00:04 +0500

    it is word count program

---
+++ word_counter/index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answer = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: chalk.bgGray("\nEnter your sentence")
+});
+let words = answer.sentence.trim().split(" ");
+console.log(words);
+console.log(chalk.bgBlue(`\n \t your sentence has  ${words.length} words`));
